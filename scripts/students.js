de108de4e3f80d1905858b6c9a5e2bcd9b8674d1
Matt McFadyen <mmcfadyen91@gmail.com>
2014-05-19 17:33:57 -0400

    added directory of students in students.js

---
+++ scripts/students.js
@@ -0,0 +1,169 @@
+var students = [
+	{
+		'id':1,
+		'name':'Jenn',
+		'fact':'Total babe',
+		'bigImg':'Jenn-260x260.jpg',  
+		
+	},
+	{
+		'id':2,
+		'name':'Emily',
+		'fact':'Knows Python',
+		'bigImg':'Emily-260x260.jpg',  
+
+	},
+	{
+		'id':3,
+		'name':'Ray',
+		'fact':'Loves Indian food',
+		'bigImg':'Ray-260x260.jpg',  
+
+	},
+	{
+		'id':4,
+		'name':'Vanessa',
+		'fact':'Basically a bunny',
+		'bigImg':'Vanessa-260x260.jpg',  
+
+	},
+	{
+		'id':5,
+		'name':'Nerissa',
+		'fact':'Is a writer',
+		'bigImg':'Nerissa-260x260.jpg',  
+
+	},
+	{
+		'id':6,
+		'name':'Rasin',
+		'fact':'Super Fashion',
+		'bigImg':'Rasin-260x260.jpg',  
+
+	},
+	{
+		'id':7,
+		'name':'Logan',
+		'fact':'Knows about snowboards',
+		'bigImg':'Logan-260x260.jpg',  
+
+	},
+	{
+		'id':8,
+		'name':'Anna',
+		'fact':'German native',
+		'bigImg':'Anna-260x260.jpg',  
+
+	},
+	{
+		'id':9,
+		'name':'Daniel',
+		'fact':'Baby on the way',
+		'bigImg':'Daniel-260x260.jpg',  
+
+	},
+	{
+		'id':10,
+		'name':'Corbin',
+		'fact':'Sweet',
+		'bigImg':'Corbin-260x260.jpg',  
+
+	},
+	{
+		'id':11,
+		'name':'Jessie',
+		'fact':'Loves bands',
+		'bigImg':'Jessie-260x260.jpg',  
+
+	},
+	{
+		'id':12,
+		'name':'Robin',
+		'fact':'Ottawa native',
+		'bigImg':'Robin-260x260.jpg',  
+
+	},
+	{
+		'id':13,
+		'name':'Jonny',
+		'fact':'Musician',
+		'bigImg':'Jonny-260x260.jpg',  
+
+	},
+	{
+		'id':14,
+		'name':'Lili',
+		'fact':'Apple genius',
+		'bigImg':'Lili-260x260.jpg',  
+
+	},
+	{
+		'id':15,
+		'name':'Marg',
+		'fact':'Dyes her hair constantly',
+		'bigImg':'Marg-260x260.jpg',  
+
+	},
+	{
+		'id':16,
+		'name':'Meahgan',
+		'fact':'Loves biking',
+		'bigImg':'Meahgan-260x260.jpg',  
+
+	},
+	{
+		'id':17,
+		'name':'Zoe',
+		'fact':'Scumbag American, Doesn\'t even lift',
+		'bigImg':'Zoe-260x260.jpg',  
+
+	},
+	{
+		'id':18,
+		'name':'Wisam',
+		'fact':'Overly nice guy',
+		'bigImg':'Wisam-260x260.jpg',  
+
+	},
+	{
+		'id':19,
+		'name':'Simon',
+		'fact':'Awesome chef',
+		'bigImg':'Simon-260x260.jpg',  
+
+	},
+	{
+		'id':20,
+		'name':'Sarah',
+		'fact':'Windows98',
+		'bigImg':'Sarah-260x260.jpg',  
+
+	},
+	{
+		'id':21,
+		'name':'Victoria',
+		'fact':'Mother of a giant can opener',
+		'bigImg':'Victoria-260x260.jpg',  
+	},
+	{
+		'id':22,
+		'name':'Matt',
+		'fact':'Is essentially a scarecrow made of hugs',
+		'bigImg':'Matt-260x260.jpg',  
+
+	},
+	{
+		'id':23,
+		'name':'Krisztina',
+		'fact': 'Graphic Designer',
+		'bigImg':'Krisztina-260x260.jpg',  
+
+	},
+	{
+		'id':24,
+		'name':'Elyse',
+		'fact':'Ottawa native',
+		'bigImg':'Elyse-260x260.jpg',  
+
+	}
+]
