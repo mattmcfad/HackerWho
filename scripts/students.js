f200e8a81ea6adc666783e2279099e34d92d158f
zkdan <zkdaniels@gmail.com>
2014-05-26 19:19:56 -0400

    added facts and match numbers

--- scripts/students.js
+++ scripts/students.js
@@ -1,81 +1,81 @@
 var students = [
 	{
 		'id':1,
-		'name':'Jenn',
-		'fact':'Total babe',
+		'name':'Jen',
+		'fact':"Her worst job was working in retail <span class='clarificaion'>(Le Chateau)</span>.",
 		'bigImg':'images/Jenn-260x260.jpg',
-		'match':'3',
+		'match':'1',
 
 		
 	},
 	{
 		'id':2,
 		'name':'Emily',
-		'fact':'Knows Python',
+		'fact':'She used to be a librarian.',
 		'bigImg':'images/Emily-260x260.jpg',
-		'match':'3',  
+		'match':'7',  
 
 	},
 	{
 		'id':3,
-		'name':'Ray',
-		'fact':'Loves Indian food',
+		'name':'Ray'
+		'fact':'The weirdest food he has eaten is grasshoppers!',
 		'bigImg':'images/Ray-260x260.jpg',
-		'match': '5' 
+		'match': '6' 
 
 	},
 	{
 		'id':4,
 		'name':'Vanessa',
-		'fact':'Basically a bunny',
+		'fact':'She used to be a librarian.',
 		'bigImg':'images/Vanessa-260x260.jpg',
-		'match': '6'  
+		'match': '7'  
 
 	},
 	{
 		'id':5,
 		'name':'Nerissa',
-		'fact':'Is a writer',
+		'fact':"She's eaten part of a cow's head. <span class='clarification'> Just the tongue, though. No big.</span>",
 		'bigImg':'images/Nerissa-260x260.jpeg',
-		'match': '1'  
+		'match': '10'  
 
 	},
 	{
 		'id':6,
 		'name':'Rasin',
-		'fact':'Super Fashion',
+		'fact':'His childhood idol was a superhero. <span class='clarificaiton'>(Superman!)</span>',
 		'bigImg':'images/Rasin-260x260.jpg',
-		'match': '1'  
+		'match': '5'  
 
 	},
 	{
 		'id':7,
 		'name':'Logan',
-		'fact':'Knows about snowboards',
+		'fact':'His childhood idol was a musician. <span class = 'clarification'> Specifically, the man in black, Johnny Cash.</span>',
 		'bigImg':'images/Logan-260x260.jpg', 
-		'match': '2' 
+		'match': '8' 
 
 	},
 	{
 		'id':8,
 		'name':'Anna',
-		'fact':'German native',
+		'fact':'She was born in Europe. <span class='clarification'> Specifically, Witzenhausen, Germany.</span>',
 		'bigImg':'images/Anna-260x260.jpg',
-		'match': '8'  
+		'match': '4'  
 
 	},
 	{
 		'id':9,
 		'name':'Daniel',
-		'fact':'Baby on the way',
+		'fact':'His joints crack. Well, mostly his thumbs. But on command!',
 		'bigImg':'images/Daniel-260x260.jpg', 
-		'match': '9' 
+		'match': '2' 
 
 	},
 	{
 		'id':10,
 		'name':'Corbin',
-		'fact':'Sweet',
+		'fact':"He is inspired by Tim Ferris.",
 		'bigImg':'images/Corbin-260x260.jpg', 
 		'match': '9' 
 
@@ -83,39 +83,39 @@ var students = [
 	{
 		'id':11,
 		'name':'Jessie',
-		'fact':'Loves bands',
+		'fact':"Her go-to kareoke song is 90's top-10 pop. <span class='clarification'>Which song? Show Me the Meaning of Being Lonely by The Backstreet Boys.</span>",
 		'bigImg':'images/Jessie-260x260.jpg',
-		'match': '10'  
+		'match': '12'  
 
 	},
 	{
 		'id':12,
 		'name':'Robin',
-		'fact':'Ottawa native',
+		'fact':"The weirdest food she has eaten is frogs' legs.",
 		'bigImg':'images/Robin-260x260.jpg',
-		'match': '12'  
+		'match': '3'  
 
 	},
 	{
 		'id':13,
 		'name':'Jonny',
-		'fact':'Jesus , Elder, Musician, GILF HUNTER',
+		'fact':"His childhood idol was a musician. <span class = 'clarification'> Specifically, Weezer's pal Buddy Holly.</span>",
 		'bigImg':'images/Jonny-260x260.jpg',  
-		'match': '12'
+		'match': '8'
 
 	},
 	{
 		'id':14,
 		'name':'Lili',
-		'fact':'Apple genius',
+		'fact':"Her go-to kareoke song is 90's top-10 pop. <span class='clarification'>Which song? My Heart Will Go On by Celine Dion.</span>",
 		'bigImg':'images/Lili-260x260.jpg',
-		'match': '10'  
+		'match': '12'  
 
 	},
 	{
 		'id':15,
-		'name':'Marg',
-		'fact':'Dyes her hair constantly',
+		'name':'Margaret',
+		'fact':"The weirdest food she has eaten is frogs' legs.",
 		'bigImg':'images/Marg-260x260.jpg', 
 		'match': '11' 
 
@@ -123,31 +123,31 @@ var students = [
 	{
 		'id':16,
 		'name':'Meahgan',
-		'fact':'Loves biking',
+		'fact':'Her childhood idol was a superhero. <span class='clarificaiton'>(She-Ra!)</span>',
 		'bigImg':'images/Meahgan-260x260.jpg', 
-		'match': '11' 
+		'match': '5' 
 
 	},
 	{
 		'id':17,
 		'name':'Zoe',
-		'fact':'Scumbag American, Doesn\'t even lift',
+		'fact':"She loves Dwayne (The Rock) Johnson. <span class='clarification'>Completely unironically.</span>",
 		'bigImg':'images/Zoe-260x260.jpg', 
-		'match': '2' 
+		'match': '11' 
 
 	},
 	{
 		'id':18,
 		'name':'Wisam',
-		'fact':'Overly nice guy',
+		'fact':"He loves Dwayne (The Rock) Johnson. <span class='clarification'>Well, he used to when he was a child.</span>",
 		'bigImg':'images/Wisam-260x260.jpg',
-		'match': '7'  
+		'match': '11'  
 
 	},
 	{
 		'id':19,
 		'name':'Simon',
-		'fact':'Awesome chef',
+		'fact':"His worst job was working in retail <span class='clarification'> (he won't say where)</span>.",
 		'bigImg':'images/Simon-260x260.jpg',
 		'match': '6'  
 
@@ -155,40 +155,40 @@ var students = [
 	{
 		'id':20,
 		'name':'Sarah',
-		'fact':'Windows98',
+		'fact':"She's eaten part of a cow's head. <span class='clarification'> Just the brains, though. No big.</span>",
 		'bigImg':'images/Sarah-260x260.jpg',
-		'match': '4' 
+		'match': '10' 
 
 	},
 	{
 		'id':21,
 		'name':'Victoria',
-		'fact':'Mother of a giant can opener',
+		'fact':'Her joints crack. <span class="clarification">Like, all of them.</span>',
 		'bigImg':'images/Victoria-260x260.jpg',
-		'match': '8'  
+		'match': '2'  
 	},
 	{
 		'id':22,
 		'name':'Matt',
-		'fact':'Is essentially a scarecrow made of hugs',
+		'fact':"He is inspired by Tim Ferris.",
 		'bigImg':'images/Matt-260x260.jpg',
-		'match': '7'  
+		'match': '9'  
 
 	},
 	{
 		'id':23,
 		'name':'Krisztina',
-		'fact': 'Graphic Designer',
+		'fact': 'She was born in Europe. <span class='clarification'> Specifically, Transylvania.</span>',
 		'bigImg':'images/Krisztina-260x260.jpg',
-		'match':'5',  
+		'match':'4',  
 
 	},
 	{
 		'id':24,
 		'name':'Elyse',
-		'fact':'Ottawa native',
+		'fact':'The weirdest food she has eaten is grasshoppers!'
 		'bigImg':'images/Elyse-260x260.jpg',
-		'match':'4'  
+		'match':'6'  
 
 	}
 ]
