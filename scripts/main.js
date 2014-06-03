ccf1d2073749a15ca7745c407e6f33aa8897dbd0
Matt McFadyen <mmcfadyen91@gmail.com>
2014-06-03 11:44:52 -0400

    de-uglified the code

--- scripts/main.js
+++ scripts/main.js
@@ -7,6 +7,7 @@ var app = {
 	init: function() {
 		app.count = 0;
 		app.matchedPairs = 0;
+
 		//hide selected sides.
 		$('.selected').css('opacity','0');
 
@@ -14,6 +15,7 @@ var app = {
 
 	},
 
+	//enable onClick method for each student
 	enableHandlers: function() {
 		//Clicking a student in the directory
 		$('.student').on('click', function(){
@@ -24,99 +26,86 @@ var app = {
 	},
 
 
+	//Upon game Completion
 	gameCompletion: function() {
-		console.log("you win");
 		//stop timer
 		window.clearInterval(countdown);
 
 		//display background
-		
 
 		//modal box to post twitter score.
+		// to do...
 	},
+
+	//Show a selected student after 1st or 2nd click
+	//@param id  - index of student
+	//@param side - left or right side to be shown (1st or 2nd click)
+	showSelected: function(id, side) {
+
+		var selected;
+		if (side === 'left') {
+			selected = $('.selected.left');
+		}
+		else if (side === 'right') {
+			selected = $('.selected.right');	
+		}
+
+		var studentObj = students[id - 1];
+
+
+		$('.student.id'+id).css('border','2px solid red');
+
+		selected.find('h2').html(studentObj.name);
+		selected.find('img').attr('src',studentObj.bigImg);
+		selected.find('p').html(studentObj.fact);
+		
+		selected.css('opacity','1');
+
+	},//showSelected
+
 
 	//Handle clicking on a student in the grid
 	//@param id - correlates to which student in students.js
 	click: function(id) {
-
-		console.log("app count:" + app.count);
-
 		
 		//cut up "students idXX" to just "XX"
-		var index = Number(id.substring(id.indexOf('id')+2));
-		//console.log("clicked! " + app.count + " index: " + index);
-		//get the student object from students array (index + 1 because of 0 indexing)
-		var studentObj = students[index -1];
-		
-		console.log("clicked: " + studentObj.name);
+		var index = Number(id.substring(id.indexOf('id') + 2));
 
 		//test if person you are clicking on is already matched.
 		if(app.matchedIds.contains(index)){
-			console.log('pick someone else!');
+			console.log('errrror: selected someone already matched!');
 		}
 
 		//dont want to select the same person.
 		else if (app.firstId === index){
-			console.log('errrror: no!');
+			console.log('errrror: selected same person!');
 		}
 
 		//2nd click
 		else if (app.count === 1){
 
-			console.log("2nd click: " + studentObj.name);
-
 			app.secondId = index;
-			//console.log("Second ID!!! : " + app.secondId);
-			//console.log("student2 id: " + index);
-			$('.student.id'+index).css('border','2px solid red');
 
-			var selectedRight = $('.selected.right');
-			
-
-			//set the h2, img and p to the appropriate student's info
-			selectedRight.find('h2').html(studentObj.name);
-			selectedRight.find('img').attr('src',studentObj.bigImg);
-			selectedRight.find('p').html(studentObj.fact);
-
-			selectedRight.css('opacity','1');
-
-			//Make the right selected VISIBLE!!!
-			
-			// selectedRight.find('h2').fadeIn(5);
-			// selectedRight.find('img').fadeIn(5);
-			// selectedRight.find('p').fadeIn(5);
+			app.showSelected(index, "right");
 			
 			//reset to first click 
 			app.count = 0;
+
 			//check if they Math
 			app.testMatch();
 		}
 
 		//first click
 		else if(app.count === 0){
+			
 			app.firstId = index;
-			
-			//console.log("first click: " + studentObj.name);
-			
-			$('.student.id'+index).css('border','2px solid red');
 
-			//fadeIn left
-			var selectedLeft = $('.selected.left');
-			selectedLeft.find('h2').html(studentObj.name);
-			selectedLeft.find('img').attr('src',studentObj.bigImg);
-			selectedLeft.find('p').html(studentObj.fact);
-			
-			$('.selected.left').css('opacity','1');
-
-			// selectedLeft.find('h2').fadeIn();
-			// selectedLeft.find('img').fadeIn();
-			// selectedLeft.find('p').fadeIn();
+			app.showSelected(index, "left");
 
 			app.count++;
 		}
 
-
-	},
+	},//click
 
 	testMatch: function() {
 
@@ -124,81 +113,42 @@ var app = {
 		var firstStudent = students[app.firstId -1];
 		var secondStudent = students[app.secondId - 1];
 
-		//remove event handlers
-		$('.student').off();
-		
-		
+		//remove event handlers so no other selection can be pressed
+		$('.student').off();	
 
 		//if they match.
 		if (firstStudent.match === secondStudent.match){
 			
-
 			app.matchedPairs++;
 			
 			//indicate they have been selected
 			$('.student.id'+app.firstId).css('background','green').addClass('matched');
-
 			$('.student.id'+app.secondId).css('background','green').addClass('matched');
 			
 			//add both the id's to the matched array
-			//this signifies they have already been matched
+			//this allows us to test if an id has been matched in the click method
 			app.matchedIds.push(app.firstId);
 			app.matchedIds.push(app.secondId);
-
-			//cache selected, delay and fade out
-		
-			// var selected = $('.selected');
-			// selected.find('h2').delay(1200).fadeOut(900);
-			// selected.find('img').delay(1200).fadeOut(900);
-			// selected.find('p').delay(1200).fadeOut(900);
 		}
 		//else deselect both
 		else {
-
 			//remove the selected border
 			$('.student.id'+app.firstId).css('border','none');
 			$('.student.id'+app.secondId).css('border','none');
-
-			//fade out both.
-			// var selected = $('.selected');
-			// selected.find('h2').delay(1200).fadeOut(900);
-			// selected.find('img').delay(1200).fadeOut(900);
-			// selected.find('p').delay(1200).fadeOut(900);
-		
 		}
 
-		//delay then re-enable event handlers.
-			// $('.student').delay(10000).on('click', function(){
-			// 	console.log("it werks");
-			// 	//class name which is "students idXX"
-			// 	var id = $(this).attr('class');
-			// 	app.click(id);
-			// });
-
-
+		//delay next selection then re-enable event handlers.
 		setTimeout(function(){
 			$('.selected').css('opacity',0);	
 			app.enableHandlers();
 		}, 1000);
 
-
-
-			// $('.selected').animate({
-			// 	opacity: 0,
-			// },
-			// 	3000, function(){
-			// 		app.enableHandlers();
-			// 	}
-			// );
-		console.log("Matched Pairs "+ app.matchedPairs);
-
+		//test game completion
 		if (app.matchedPairs === 12){
 			app.gameCompletion();
 		}
 
-
 	},//test Match
-
 
 }//app
 
